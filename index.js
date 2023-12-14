b7761ba6a8ca1666f91e5493d4154a86202b0053
asib ahmed <ahmedasib1@gmail.com>
2023-12-14 23:21:02 +0600

    delete data

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)}
       const options = {
-        projection: {title: 1, price: 1, service_id: 1},
+        projection: {title: 1, price: 1, service_id: 1, img: 1},
       };
 
       const result = await serviceCollection.findOne(query, options);
@@ -54,12 +54,19 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/bookings', (req, res) =>{
+    app.post('/bookings', async(req, res) =>{
       const booking = req.body;
       console.log(booking)
-      const result = bookingCollection.insertOne(booking);
+      const result =await bookingCollection.insertOne(booking);
       res.send(result)
     })
+
+    app.delete('/bookings/:id',async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result =await bookingCollection.deleteOne(query);
+      res.send(result);
+    } )
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
