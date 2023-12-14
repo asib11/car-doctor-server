4b0c3870d3257b848003995721706a0853bce7bb
asib ahmed <ahmedasib1@gmail.com>
2023-12-14 15:34:22 +0600

    booking query make

--- index.js
+++ index.js
@@ -45,6 +45,15 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/bookings', async(req, res) =>{
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.post('/bookings', (req, res) =>{
       const booking = req.body;
       console.log(booking)
