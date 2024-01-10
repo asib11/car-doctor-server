23b4992489d4c6ef7ef43907cd0f564d2726e49b
asib ahmed <ahmedasib1@gmail.com>
2024-01-10 23:26:52 +0600

    add decoded

--- index.js
+++ index.js
@@ -73,6 +73,11 @@ async function run() {
 
     app.get('/bookings', verifyJWT, async(req, res) =>{
       // console.log(req.headers.authorization);
+      const decoded = req.decoded;
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: 1, message: 'forbidden access'})
+      }
+      
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
